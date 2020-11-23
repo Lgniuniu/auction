262b2013a7ef6440912f4b817c5589b2d8c832f7
15059189360 <807050013@qq.com>
2020-11-23 09:28:50 +0800

    11.23

---
+++ src/main/java/com/how2java/tmall/es/ProductESDAO.java
@@ -0,0 +1,13 @@
+
+
+package com.how2java.tmall.es;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import com.how2java.tmall.pojo.Product;
+
+public interface ProductESDAO extends ElasticsearchRepository<Product,Integer>{
+
+}
+
+
